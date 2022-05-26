f8367988edc85e13cd5e42dba66cf44b19b1f789
sawancse <sawancse@gmail.com>
2022-05-26 08:43:52 +0530

    Add files via upload

---
+++ learncamel-spring-boot-sql/learncamel-spring-boot-sql/src/main/java/com/learncamel/route/SimpleCamelRoute.java
@@ -0,0 +1,50 @@
+package com.learncamel.route;
+
+import com.learncamel.domain.Item;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.dataformat.bindy.csv.BindyCsvDataFormat;
+import org.apache.camel.spi.DataFormat;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.stereotype.Component;
+
+import java.util.logging.Logger;
+
+
+@Component
+@Slf4j
+public class SimpleCamelRoute  extends RouteBuilder{
+
+
+    @Autowired
+    Environment environment;
+
+
+    @Override
+    public void configure() throws Exception {
+
+        log.info("Starting the Camel Route");
+
+        DataFormat bindy = new BindyCsvDataFormat(Item.class);
+
+        from("{{startRoute}}")
+                .log("Timer Invoked and the body" + environment.getProperty("message"))
+                .choice()
+                    .when((header("env").isNotEqualTo("mock")))
+                        .pollEnrich("{{fromRoute}}")
+                    .otherwise()
+                        .log("mock env flow and the body is ${body}")
+                .end()
+                .to("{{toRoute1}}")
+                .unmarshal(bindy)
+                .log("The unmarshaled object is ${body}")
+                .split(body())
+                    .log("Record is ${body}")
+                .end();
+
+        log.info("Ending the Camel Route");
+
+
+    }
+}
