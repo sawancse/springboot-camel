0f3650ba0a1f2ecc610bbcc0a57e44c3511c7fbe
sawancse <sawancse@gmail.com>
2022-05-26 08:36:17 +0530

    Add files via upload

---
+++ simple-rest-api/src/main/java/com/learnspringboot/service/CurrentTimeService.java
@@ -0,0 +1,20 @@
+package com.learnspringboot.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+ 
+@Service
+public class CurrentTimeService {
+
+    @Autowired
+    Environment environment;
+
+    public String getCurrentDateTime() {
+
+        LocalDateTime localDateTime = LocalDateTime.now();
+        return environment.getProperty("message").concat("\n").concat(localDateTime.toString());
+    }
+}
